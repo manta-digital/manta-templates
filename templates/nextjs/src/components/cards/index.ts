2dcb711f6d156eede5c11e22562fd83ec0b73dc2
erik corkran <erik.corkran@manta.digital>
2025-08-24 07:52:45 -0600

    Cleanup templates/nextjs: remove unused cards 1

--- templates/nextjs/src/components/cards/index.ts
+++ templates/nextjs/src/components/cards/index.ts
@@ -3,11 +3,6 @@
 export { BaseCard } from './BaseCard';
 export { default as BlogCard } from './BlogCard';
 export { default as BlogCardWide } from './BlogCardWide';
-export { default as ProjectCard } from './ProjectCard';
 export { default as QuoteCard } from './QuoteCard';
-export { default as SidebarPostCard } from './SidebarPostCard';
 export { default as BlogCardImage } from './BlogCardImage';
-export { default as ComingSoonFeatureCard } from './ComingSoonFeatureCard';
-export { default as GuidesFeatureCard } from './GuidesFeatureCard';
-export { default as ThreeJSCard } from './ThreeJSCard';
 export { default as CosineTerrainCard } from './math/CosineTerrainCard';
